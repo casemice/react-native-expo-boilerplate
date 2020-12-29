59b2d0ba81b9fc31a031f5f6e1b25d78e9bfa3ff
enesozturk <enesozturk.d@gmail.com>
2020-12-29 13:45:17 +0300

    Add testing with jest and renderer

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+  ],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'ios.ts',
+    'ios.tsx',
+    'android.ts',
+    'android.tsx',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/index.tsx',
+    '!src/setupTests.ts',
+    '!src/components/**/index.{ts,tsx}',
+    '!src/**/*.stories.{ts,tsx}',
+    '!src/**/*.style.ts',
+    '!src/styles/**/*',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
